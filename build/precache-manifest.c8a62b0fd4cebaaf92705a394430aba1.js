aaf4c637115dc6f4f64cfcca6d6429c7c58584ff
陈凯华 <44825567+meiliduizhang@users.noreply.github.com>
2020-10-24 07:42:36 +0800

    任务三

---
+++ build/precache-manifest.c8a62b0fd4cebaaf92705a394430aba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab8ebbbed2a90ee0111c67347a8ea63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fd2e983474db01742db",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "ce04c65378019084b34f",
+    "url": "./static/css/main.c022b1c5.chunk.css"
+  },
+  {
+    "revision": "3fd2e983474db01742db",
+    "url": "./static/js/2.0c988123.chunk.js"
+  },
+  {
+    "revision": "3530bbda1c47bd33b8cccf03b351d584",
+    "url": "./static/js/2.0c988123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce04c65378019084b34f",
+    "url": "./static/js/main.7b1978c4.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
